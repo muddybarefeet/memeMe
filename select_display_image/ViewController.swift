67038a500de635a483056982bc904189c80ac57b
Anna Rogers <2muddybarefeet@gmail.com>
2016-07-04 09:20:54 -0700

    working on the save button

--- select_display_image/ViewController.swift
+++ select_display_image/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
-
-
+    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
     @IBOutlet weak var imagePickerView: UIImageView!
-    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
+    @IBOutlet weak var topToolbar: UIToolbar!
+
+    @IBOutlet weak var toolbar: UIToolbar!
+    
     
 //    name the delegates
     let topTextDelegate = TextFieldDelegate()
@@ -76,6 +80,7 @@ UINavigationControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        shareButton.enabled = false
         imagePickerView.contentMode = UIViewContentMode.ScaleAspectFill
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
         topText.text = "TOP"
@@ -107,14 +112,57 @@ UINavigationControllerDelegate {
     }
     
     
-    @IBAction func pickAnImageFromCamera(sender: AnyObject) {
-        let imagePickerCamera = UIImagePickerController()
-        imagePickerCamera.delegate = self
-        imagePickerCamera.sourceType = UIImagePickerControllerSourceType.Camera
-        presentViewController(imagePickerCamera, animated:true, completion:nil)
-        
+    @IBAction func takeAPicture(sender: AnyObject) {
+        let camera = UIImagePickerController()
+        camera.delegate = self
+        camera.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(camera, animated: true, completion: nil)
     }
-
+    
+    struct Meme {
+        var topString: String
+        var bottomString: String
+        var originalImage: UIImage
+        var memedImage: UIImage
+    }
+    
+    func save(memedImage: UIImage) {
+        
+        let meme = Meme(topString: topText.text, bottomString: bottomText.text, originalImage: imagePickerView.image, memedImage: memedImage)
+        //TODO: Add to memes array in AppDelegate
+    }
+    
+    @IBAction func shareMemeButtonPressed(sender: UIBarButtonItem) {
+        
+        let memedImage = generateMemedImage()
+        let activityViewController = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        presentViewController(activityViewController, animated: true, completion: nil)
+        
+        activityViewController.completionWithItemsHandler = {
+            (activity: String?, completed: Bool, items: [AnyObject]?, error: NSError?) -> Void in
+            if completed {
+                self.save(memedImage)
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide toolbar and navbar
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawViewHierarchyInRect(self.view.frame,
+                                     afterScreenUpdates: true)
+        let memedImage : UIImage =
+            UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // TODO:  Show toolbar and navbar       
+        
+        return memedImage
+    }
 
 }
 
